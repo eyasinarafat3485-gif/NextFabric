1ba090189d73b6bc0950c598ffcf1f2b66b44d67
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-07-19 10:36:53 +0600

    loading text color change

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,7 @@ export default function Loading() {
         <div className=" inset-0 z-50 min-h-screen flex flex-col items-center justify-center bg-zinc-950/70 ">
             <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
 
-            <p className="mt-4 text-xl font-semibold text-red-500 dark:text-gray-200 animate-pulse">
+            <p className="mt-4 text-xl font-semibold text-gray-500 dark:text-gray-200 animate-pulse">
                 Loading....
             </p>
         </div>
